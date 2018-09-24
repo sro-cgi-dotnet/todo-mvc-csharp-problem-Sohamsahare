269ce1bc2d5ad988f7a82dd2577db2a89ce69d7c
Soham Sahare <sohamsahare@gmail.com>
2018-09-24 10:28:42 +0530

    fixed format

--- TodoApi.Tests/TodoApiTests.cs
+++ TodoApi.Tests/TodoApiTests.cs
@@ -8,255 +8,283 @@ using TodoApi.Controllers;
 using TodoApi.Models;
 using Xunit;
 
-namespace TodoApi.Tests {
-    public class TodoApiTests {
+namespace TodoApi.Tests
+{
+    public class TodoApiTests
+    {
         // List of notes used as a fake database
-        private List<Note> GetMockDatabase () {
+        private List<Note> GetMockDatabase()
+        {
             return new List<Note> {
-                new Note {
+                new Note
+                {
                     NoteId = 1,
-                        Title = "Things to do",
-                        CheckList = new List<CheckListItem> {
-                            new CheckListItem {
+                    Title = "Things to do",
+                    CheckList = new List<CheckListItem> {
+                        new CheckListItem {
                             Id = 1,
                             Text = "Submit assignment before 6:00PM",
                             NoteId = 1
-                            }
-                            },
-                            Labels = new List<Label> {
-                            new Label {
+                        }
+                    },
+                    Labels = new List<Label> {
+                        new Label {
                             Id = 1,
                             Name = "ASP.NETCore"
-                            }
-                            }
-                            },
-                            new Note {
-                            NoteId = 2,
-                            Title = "Trial",
-                            CheckList = new List<CheckListItem> {
-                            new CheckListItem {
+                        }
+                    }
+                },
+                new Note {
+                    NoteId = 2,
+                    title = "Trial",
+                    CheckList = new List<CheckListItem> {
+                        new CheckListItem {
                             Id = 2,
                             Text = "Try Xunit",
                             NoteId = 2
-                            }
-                            },
-                            Labels = new List<Label> {
-                            new Label {
+                        }
+                    },
+                    Labels = new List<Label> {
+                        new Label {
                             Id = 2,
                             Name = "Trial"
-                            }
-                            }
-                            }
+                        }
+                    }
+                }
             };
         }
 
         [Fact]
-        public void GetAll_Positive_ListWithEntries () {
-            var datarepo = new Mock<IDataRepo> ();
-            List<Note> notes = GetMockDatabase ();
-            datarepo.Setup (d => d.GetAllNotes ()).Returns (notes);
-            TodoController todoController = new TodoController (datarepo.Object);
+        public void GetAll_Positive_ListWithEntries()
+        {
+            var datarepo = new Mock<IDataRepo>();
+            List<Note> notes = GetMockDatabase();
+            datarepo.Setup(d => d.GetAllNotes()).Returns(notes);
+            TodoController todoController = new TodoController(datarepo.Object);
 
-            var actionResult = todoController.Get ();
+            var actionResult = todoController.Get();
 
             var okObjectResult = actionResult as OkObjectResult;
-            Assert.NotNull (okObjectResult);
+            Assert.NotNull(okObjectResult);
 
             var model = okObjectResult.Value as List<Note>;
-            Assert.NotNull (model);
+            Assert.NotNull(model);
 
-            Assert.Equal (notes.Count, model.Count);
+            Assert.Equal(notes.Count, model.Count);
         }
 
         [Fact]
-        public void GetAll_Negative_EmptyList () {
-            var datarepo = new Mock<IDataRepo> ();
-            List<Note> notes = new List<Note> ();
+        public void GetAll_Negative_EmptyList()
+        {
+            var datarepo = new Mock<IDataRepo>();
+            List<Note> notes = new List<Note>();
 
-            datarepo.Setup (d => d.GetAllNotes ()).Returns (notes);
-            TodoController todoController = new TodoController (datarepo.Object);
-            var actionResult = todoController.Get ();
+            datarepo.Setup(d => d.GetAllNotes()).Returns(notes);
+            TodoController todoController = new TodoController(datarepo.Object);
+            var actionResult = todoController.Get();
 
             var okObjectResult = actionResult as OkObjectResult;
-            Assert.NotNull (okObjectResult);
+            Assert.NotNull(okObjectResult);
 
             var model = okObjectResult.Value as List<Note>;
-            Assert.NotNull (model);
+            Assert.NotNull(model);
 
-            Assert.Equal (notes.Count, model.Count);
+            Assert.Equal(notes.Count, model.Count);
         }
 
         [Fact]
-        public void GetAll_Negative_DatabaseError () {
-            var datarepo = new Mock<IDataRepo> ();
+        public void GetAll_Negative_DatabaseError()
+        {
+            var datarepo = new Mock<IDataRepo>();
             List<Note> notes = null;
-            datarepo.Setup (d => d.GetAllNotes ()).Returns (notes);
-            TodoController todoController = new TodoController (datarepo.Object);
-            var result = todoController.Get ();
-            Assert.IsType<NotFoundObjectResult> (result);
+            datarepo.Setup(d => d.GetAllNotes()).Returns(notes);
+            TodoController todoController = new TodoController(datarepo.Object);
+            var result = todoController.Get();
+            Assert.IsType<NotFoundObjectResult>(result);
         }
 
         [Fact]
-        public void GetById_Positive_ReturnsNoteWithId1 () {
-            var datarepo = new Mock<IDataRepo> ();
-            List<Note> notes = GetMockDatabase ();
+        public void GetById_Positive_ReturnsNoteWithId1()
+        {
+            var datarepo = new Mock<IDataRepo>();
+            List<Note> notes = GetMockDatabase();
             int id = 1;
-            datarepo.Setup (d => d.GetNote (id)).Returns (notes.Find (n => n.NoteId == id));
-            TodoController todoController = new TodoController (datarepo.Object);
-            var actionResult = todoController.Get (id);
+            datarepo.Setup(d => d.GetNote(id)).Returns(notes.Find(n => n.NoteId == id));
+            TodoController todoController = new TodoController(datarepo.Object);
+            var actionResult = todoController.Get(id);
 
             var okObjectResult = actionResult as OkObjectResult;
-            Assert.NotNull (okObjectResult);
+            Assert.NotNull(okObjectResult);
 
             var model = okObjectResult.Value as Note;
-            Assert.NotNull (model);
+            Assert.NotNull(model);
 
-            Assert.Equal (id, model.NoteId);
+            Assert.Equal(id, model.NoteId);
         }
 
         [Fact]
-        public void GetById_Negative_ReturnsNullNotFound () {
-            var datarepo = new Mock<IDataRepo> ();
-            List<Note> notes = GetMockDatabase ();
+        public void GetById_Negative_ReturnsNullNotFound()
+        {
+            var datarepo = new Mock<IDataRepo>();
+            List<Note> notes = GetMockDatabase();
             int id = 3;
-            datarepo.Setup (d => d.GetNote (id)).Returns (notes.Find (n => n.NoteId == id));
-            TodoController todoController = new TodoController (datarepo.Object);
-            var result = todoController.Get (id);
-            Assert.IsType<NotFoundObjectResult> (result);
+            datarepo.Setup(d => d.GetNote(id)).Returns(notes.Find(n => n.NoteId == id));
+            TodoController todoController = new TodoController(datarepo.Object);
+            var result = todoController.Get(id);
+            Assert.IsType<NotFoundObjectResult>(result);
         }
 
         [Fact]
-        public void GetByTitle_Positive_ReturnsNoteWithTitle () {
-            var datarepo = new Mock<IDataRepo> ();
-            List<Note> notes = GetMockDatabase ();
+        public void GetByTitle_Positive_ReturnsNoteWithTitle()
+        {
+            var datarepo = new Mock<IDataRepo>();
+            List<Note> notes = GetMockDatabase();
             int expected = 1;
             string type = Constant.Type.Title;
             string text = "Trial";
-            datarepo.Setup (d => d.GetNote (text, type)).Returns (notes.FindAll (n => n.Title == text));
-            TodoController todoController = new TodoController (datarepo.Object);
-            var actionResult = todoController.Get (text, type);
+            datarepo.Setup(d => d.GetNote(text, type)).Returns(notes.FindAll(n => n.Title == text));
+            TodoController todoController = new TodoController(datarepo.Object);
+            var actionResult = todoController.Get(text, type);
 
             var okObjectResult = actionResult as OkObjectResult;
-            Assert.NotNull (okObjectResult);
+            Assert.NotNull(okObjectResult);
 
             var model = okObjectResult.Value as List<Note>;
-            Assert.NotNull (model);
+            Assert.NotNull(model);
 
-            Assert.Equal (expected, model.Count);
+            Assert.Equal(expected, model.Count);
         }
 
         [Fact]
-        public void GetByTitle_Negative_ReturnsNotFound() {
-            var datarepo = new Mock<IDataRepo> ();
-            List<Note> notes = GetMockDatabase ();
+        public void GetByTitle_Negative_ReturnsNotFound()
+        {
+            var datarepo = new Mock<IDataRepo>();
+            List<Note> notes = GetMockDatabase();
             string type = Constant.Type.Title;
             string text = "NonExistentTitle";
-            datarepo.Setup (d => d.GetNote (text, type)).Returns (notes.FindAll(n=>n.Title == text));
-            TodoController todoController = new TodoController (datarepo.Object);
-            var actionResult = todoController.Get (text, type);
+            datarepo.Setup(d => d.GetNote(text, type)).Returns(notes.FindAll(n => n.Title == text));
+            TodoController todoController = new TodoController(datarepo.Object);
+            var actionResult = todoController.Get(text, type);
 
             var nfObjectResult = actionResult as NotFoundObjectResult;
-            Assert.NotNull (nfObjectResult);
+            Assert.NotNull(nfObjectResult);
         }
+
         [Fact]
-        public void GetByTitle_Negative_ReturnsBadRequest() {
-            var datarepo = new Mock<IDataRepo> ();
+        public void GetByTitle_Negative_ReturnsBadRequest()
+        {
+            var datarepo = new Mock<IDataRepo>();
             List<Note> notes = null;
             string type = "NonExistentType";
             string text = "NonExistentTitle";
-            datarepo.Setup (d => d.GetNote (text, type)).Returns (notes);
-            TodoController todoController = new TodoController (datarepo.Object);
-            var actionResult = todoController.Get (text, type);
+            datarepo.Setup(d => d.GetNote(text, type)).Returns(notes);
+            TodoController todoController = new TodoController(datarepo.Object);
+            var actionResult = todoController.Get(text, type);
 
             var brObjectResult = actionResult as BadRequestObjectResult;
-            Assert.NotNull (brObjectResult);
+            Assert.NotNull(brObjectResult);
         }
+
         [Fact]
-        public void PostById_Positive_ReturnsCreated() {
-            var datarepo = new Mock<IDataRepo> ();
-            Note note = new Note{
+        public void PostById_Positive_ReturnsCreated()
+        {
+            var datarepo = new Mock<IDataRepo>();
+            Note note = new Note
+            {
                 NoteId = 4,
                 Title = "Testing Post"
             };
-            datarepo.Setup (d => d.PostNote(note)).Returns (true);
-            TodoController todoController = new TodoController (datarepo.Object);
+            datarepo.Setup(d => d.PostNote(note)).Returns(true);
+            TodoController todoController = new TodoController(datarepo.Object);
             var actionResult = todoController.Post(note);
 
             var crObjectResult = actionResult as CreatedResult;
-            Assert.NotNull (crObjectResult);
+            Assert.NotNull(crObjectResult);
 
             var model = crObjectResult.Value as Note;
-            Assert.Equal(note.NoteId,model.NoteId);
+            Assert.Equal(note.NoteId, model.NoteId);
         }
+
         [Fact]
-        public void PostById_Negative_ReturnsBadRequest() {
-            var datarepo = new Mock<IDataRepo> ();
-            Note note = new Note{
+        public void PostById_Negative_ReturnsBadRequest()
+        {
+            var datarepo = new Mock<IDataRepo>();
+            Note note = new Note
+            {
                 NoteId = 4,
                 Title = "Testing Post"
             };
-            datarepo.Setup (d => d.PostNote(note)).Returns (false);
-            TodoController todoController = new TodoController (datarepo.Object);
+            datarepo.Setup(d => d.PostNote(note)).Returns(false);
+            TodoController todoController = new TodoController(datarepo.Object);
             var actionResult = todoController.Post(note);
 
             var brObjectResult = actionResult as BadRequestObjectResult;
-            Assert.NotNull (brObjectResult);
+            Assert.NotNull(brObjectResult);
         }
+
         [Fact]
-        public void PutById_Positive_ReturnsCreated() {
-            var datarepo = new Mock<IDataRepo> ();
-            Note note = new Note{
+        public void PutById_Positive_ReturnsCreated()
+        {
+            var datarepo = new Mock<IDataRepo>();
+            Note note = new Note
+            {
                 NoteId = 4,
                 Title = "Testing Post"
             };
             int id = (int)note.NoteId;
-            datarepo.Setup (d => d.PutNote(id,note)).Returns (true);
-            TodoController todoController = new TodoController (datarepo.Object);
-            var actionResult = todoController.Put(id,note);
+            datarepo.Setup(d => d.PutNote(id, note)).Returns(true);
+            TodoController todoController = new TodoController(datarepo.Object);
+            var actionResult = todoController.Put(id, note);
 
             var crObjectResult = actionResult as CreatedResult;
-            Assert.NotNull (crObjectResult);
+            Assert.NotNull(crObjectResult);
 
             var model = crObjectResult.Value as Note;
-            Assert.Equal(id,model.NoteId);
+            Assert.Equal(id, model.NoteId);
         }
+
         [Fact]
-        public void PutById_Negative_ReturnsNotFoundt() {
-            var datarepo = new Mock<IDataRepo> ();
-            Note note = new Note{
+        public void PutById_Negative_ReturnsNotFoundt()
+        {
+            var datarepo = new Mock<IDataRepo>();
+            Note note = new Note
+            {
                 NoteId = 4,
                 Title = "Testing Post"
             };
             int id = (int)note.NoteId;
-            datarepo.Setup (d => d.PostNote(note)).Returns (false);
-            TodoController todoController = new TodoController (datarepo.Object);
-            var actionResult = todoController.Put(id,note);
+            datarepo.Setup(d => d.PostNote(note)).Returns(false);
+            TodoController todoController = new TodoController(datarepo.Object);
+            var actionResult = todoController.Put(id, note);
 
             var nfObjectResult = actionResult as NotFoundObjectResult;
-            Assert.NotNull (nfObjectResult);
+            Assert.NotNull(nfObjectResult);
         }
 
-                [Fact]
-        public void DeleteById_Positive_ReturnsCreated() {
-            var datarepo = new Mock<IDataRepo> ();
+        [Fact]
+        public void DeleteById_Positive_ReturnsCreated()
+        {
+            var datarepo = new Mock<IDataRepo>();
             int id = 1;
-            datarepo.Setup (d => d.DeleteNote(id)).Returns (true);
-            TodoController todoController = new TodoController (datarepo.Object);
+            datarepo.Setup(d => d.DeleteNote(id)).Returns(true);
+            TodoController todoController = new TodoController(datarepo.Object);
             var actionResult = todoController.Delete(id);
 
             var okObjectResult = actionResult as OkObjectResult;
-            Assert.NotNull (okObjectResult);
+            Assert.NotNull(okObjectResult);
         }
+
         [Fact]
-        public void DeleteById_Negative_ReturnsNotFoundt() {
-            var datarepo = new Mock<IDataRepo> ();
+        public void DeleteById_Negative_ReturnsNotFoundt()
+        {
+            var datarepo = new Mock<IDataRepo>();
             int id = 5;
-            datarepo.Setup (d => d.DeleteNote(id)).Returns (false);
-            TodoController todoController = new TodoController (datarepo.Object);
+            datarepo.Setup(d => d.DeleteNote(id)).Returns(false);
+            TodoController todoController = new TodoController(datarepo.Object);
             var actionResult = todoController.Delete(id);
 
             var nfObjectResult = actionResult as NotFoundObjectResult;
-            Assert.NotNull (nfObjectResult);
+            Assert.NotNull(nfObjectResult);
         }
     }
 }
