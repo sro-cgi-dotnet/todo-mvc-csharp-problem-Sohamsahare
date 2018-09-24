2ee55aca916bb14941293fd59fce6e8ee275f7f1
Soham Sahare <sohamsahare@gmail.com>
2018-09-24 12:27:15 +0530

    Commit for Travis update

--- TodoApi/Constants.cs
+++
@@ -1,9 +0,0 @@
-namespace TodoApi{
-    public static class Constant{
-        public static class Type{
-            public static string Title = "title";
-            public static string Label = "label";
-            public static string Pinned = "pinned";
-        }
-    }
-}
